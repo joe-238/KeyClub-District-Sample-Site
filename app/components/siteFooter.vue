64e35695ee52dd5aab2394fa169324d8e3138336
joe-238 <171643026+joe-238@users.noreply.github.com>
2026-04-05 20:28:33 -0400

    sss

--- app/components/siteFooter.vue
+++ app/components/siteFooter.vue
@@ -2,7 +2,7 @@
   <footer
     class="bg-gray-100 text-center py-4 sm:py-6 lg:py-8 text-xs sm:text-sm lg:text-base text-gray-500"
   >
-    Created as a sample Key Club district website by
+    Created by
     <a
       href="https://github.com/joe-238"
       target="_blank"
